4cf12a425e27c72bf14017e05e3f07b84a047ee5
Even Holmen <Even Holmen>
2018-04-22 21:13:54 +0200

    Revert "kommentarer"
    This reverts commit 4a6dee82122ec945143709811824b40fdbc22945.

--- src/app.js
+++ src/app.js
@@ -1475,7 +1475,7 @@ class KalenderDetaljer extends React.Component {
           <p ref="arrangementKnappeP">
             <span ref="arrKnapperSpan"></span>
             <br />
-            <span ref=></span>
+            <span ref="interesseKnappSpan"></span>
           </p>
         </div>
         <div ref="arrangementMannskapDiv" className="arrangementDetaljerDiv">
@@ -1809,8 +1809,7 @@ class KalenderDetaljer extends React.Component {
               datoString = datoArray[3]+"-"+month+"-"+datoArray[2];
             }
 
-            //Henter alle aktiverte brukere som ikke er passive når arrangementdatoen er
-            //og viser dem fram i en liste basert på vaktpoeng
+            //Henter alle aktiverte brukere som ikke er passive når arrangementdatoen er og viser dem fram i en liste
             //Gir mulighet til å melde bruker opp til arrangement hvis det ikke er nok meldt opp
             this.refs.arrangementMannskapSokListe.innerText = " ";
             this.refs.arrangementMannskapSokKnapp.onclick = () => {
